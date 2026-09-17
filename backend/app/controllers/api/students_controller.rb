3b3a691868dfacac3cee7b6118a957cb4bd578c4
Luiz F. Picolo <luizpicolo@gmail.com>
2026-09-17 15:28:23 -0400

    feat: add student creation API

--- backend/app/controllers/api/students_controller.rb
+++ backend/app/controllers/api/students_controller.rb
@@ -26,6 +26,17 @@ class Api::StudentsController < ApplicationController
     render json: { student: student_json(@student, detailed: true) }
   end
 
+  # POST /api/students
+  def create
+    student = Student.new(student_params)
+
+    if student.save
+      render json: { student: student_json(student, detailed: true) }, status: :created
+    else
+      render json: { errors: student.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
   # PATCH/PUT /api/students/:id
   def update
     attributes = student_params
@@ -36,6 +47,18 @@ class Api::StudentsController < ApplicationController
     else
       render json: { errors: @student.errors.full_messages }, status: :unprocessable_entity
     end
+  end
+
+  # GET /api/students/options
+  def options
+    courses = Course.where(set_polo).order(:name)
+    school_groups = SchoolGroup.where(polo_id: courses.select(:polo_id)).order(:identifier, :name)
+
+    render json: {
+      courses: courses.as_json(only: %i[id name initial]),
+      school_groups: school_groups.as_json(only: %i[id name identifier polo_id]),
+      course_situations: Student.course_situations.keys
+    }
   end
 
   private
@@ -94,7 +117,12 @@ class Api::StudentsController < ApplicationController
       :responsible_contact,
       :contact,
       :password,
-      :password_confirmation
+      :password_confirmation,
+      :ra,
+      :enrollment,
+      :course_situation,
+      :course_id,
+      :school_group_id
     )
   end
 
