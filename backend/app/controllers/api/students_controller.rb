38edd64d7d391e7c0e2ca64c5c92c9da9964874d
Luiz F. Picolo <luizpicolo@gmail.com>
2026-09-17 15:12:45 -0400

    feat: add student detail API

--- backend/app/controllers/api/students_controller.rb
+++ backend/app/controllers/api/students_controller.rb
@@ -4,6 +4,7 @@ class Api::StudentsController < ApplicationController
   include ParamsSearch
 
   before_action :authenticate_user!
+  before_action :set_student, only: :show
 
   # GET /api/students
   def index
@@ -18,15 +19,45 @@ class Api::StudentsController < ApplicationController
       students: students.as_json(
         only: %i[id name ra enrollment photo course_situation],
         include: {
-          course: { only: %i[id name initial] },
-          school_group: { only: %i[id name] }
+          course: {
+            only: %i[id name initial polo_id],
+            include: { polo: { only: %i[id name] } }
+          },
+          school_group: {
+            only: %i[id name identifier polo_id],
+            include: { polo: { only: %i[id name] } }
+          }
         }
       ),
       total: students.total_count
     }
   end
 
+  # GET /api/students/:id
+  def show
+    render json: {
+      student: @student.as_json(
+        only: %i[id name cpf birth_date responsible responsible_contact contact ra enrollment course_situation created_at updated_at photo],
+        methods: :course_situation,
+        include: {
+          course: {
+            only: %i[id name initial polo_id],
+            include: { polo: { only: %i[id name] } }
+          },
+          school_group: {
+            only: %i[id name identifier polo_id],
+            include: { polo: { only: %i[id name] } }
+          }
+        }
+      )
+    }
+  end
+
   private
+
+  def set_student
+    @student = Student.includes(course: :polo, school_group: :polo).find(params[:id])
+  end
 
   def params_return
     return {} if set_polo.empty?
