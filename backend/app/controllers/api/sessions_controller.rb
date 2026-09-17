5a26e2ec70659d81afe42af7f3e901a6bdf45d83
Luiz F. Picolo <luizpicolo@gmail.com>
2026-09-17 14:54:14 -0400

    fix(api): handle login requests and return JWT

--- backend/app/controllers/api/sessions_controller.rb
+++ backend/app/controllers/api/sessions_controller.rb
@@ -1,16 +1,30 @@
 class Api::SessionsController < Devise::SessionsController
   layout false
-  protect_from_forgery with: :null_session
+  skip_before_action :verify_authenticity_token
   respond_to :json
 
-  # Adicione este método:
   def create
-    user = User.find_for_database_authentication(username: params[:user][:username])
-    if user&.valid_password?(params[:user][:password])
-      p sign_in(:user, user)
-      render json: { user: user, message: 'Login realizado com sucesso' }, status: :ok
-    else
+    credentials = params.require(:user).permit(:username, :password)
+    user = User.find_for_database_authentication(username: credentials[:username])
+
+    unless user&.valid_password?(credentials[:password])
       render json: { error: 'Usuário ou senha inválidos' }, status: :unauthorized
+      return
     end
+
+    sign_in(:user, user)
+    token = JWT.encode(
+      {
+        sub: user.id,
+        exp: 30.minutes.from_now.to_i
+      },
+      Rails.application.credentials.jwt_secret_key,
+      'HS256'
+    )
+
+    response.set_header('Authorization', "Bearer #{token}")
+    render json: { user: user, message: 'Login realizado com sucesso' }, status: :ok
+  rescue ActionController::ParameterMissing
+    render json: { error: 'Usuário e senha são obrigatórios' }, status: :bad_request
   end
 end
