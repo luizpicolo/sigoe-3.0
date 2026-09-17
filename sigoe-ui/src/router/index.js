1aab91dfcf4994f0bb96542f4a03577698d3bc7a
Luiz F. Picolo <luizpicolo@gmail.com>
2026-09-17 17:19:37 -0400

    feat: protect Vue routes with permissions

--- sigoe-ui/src/router/index.js
+++ sigoe-ui/src/router/index.js
@@ -1,8 +1,10 @@
 import { createRouter, createWebHistory } from "vue-router"
 import { isTokenValid } from "@/services/authentication"
+import { can, loadCurrentPermissions } from "@/services/permissions"
 
 import Home from "@/views/home.vue"
 import Login from "@/views/login.vue"
+import Forbidden from "@/views/forbidden.vue"
 import UserList from "@/views/users/list.vue"
 import UserView from "@/views/users/show.vue"
 import UserNew from "@/views/users/new.vue"
@@ -23,30 +25,31 @@ import ChangePassword from "@/views/users/change-password.vue"
 const routes = [
   { path: "/", component: Login, meta: { auth: false } },
   { path: "/home", component: Home, meta: { auth: true } },
+  { path: "/sem-permissao", component: Forbidden, meta: { auth: true } },
 
   // Usuários
-  { path: "/administrador/usuarios/listar", component: UserList, meta: { auth: true } },
-  { path: "/administrador/usuarios/visualizar/:id", component: UserView, meta: { auth: true } },
-  { path: "/administrador/usuarios/novo", component: UserNew, meta: { auth: true } },
-  { path: "/administrador/usuarios/permissoes/:id/", component: UserPermissions, meta: { auth: true } },
+  { path: "/administrador/usuarios/listar", component: UserList, meta: { auth: true, permission: { entity: "users", action: "read" } } },
+  { path: "/administrador/usuarios/visualizar/:id", component: UserView, meta: { auth: true, permission: { entity: "users", action: "read" } } },
+  { path: "/administrador/usuarios/novo", component: UserNew, meta: { auth: true, permission: { entity: "users", action: "create" } } },
+  { path: "/administrador/usuarios/permissoes/:id/", component: UserPermissions, meta: { auth: true, permission: { entity: "users", action: "update" } } },
   { path: "/administrador/usuarios/trocar-senha", component: ChangePassword, meta: { auth: true } },
 
   // Estudantes
-  { path: "/administrador/estudantes/listar", component: StudentList, meta: { auth: true } },
-  { path: "/administrador/estudantes/novo", component: StudentNew, meta: { auth: true } },
-  { path: "/administrador/estudantes/visualizar/:id", component: StudentView, meta: { auth: true } },
-  { path: "/administrador/estudantes/editar/:id", component: StudentEdit, meta: { auth: true } },
+  { path: "/administrador/estudantes/listar", component: StudentList, meta: { auth: true, permission: { entity: "students", action: "read" } } },
+  { path: "/administrador/estudantes/novo", component: StudentNew, meta: { auth: true, permission: { entity: "students", action: "create" } } },
+  { path: "/administrador/estudantes/visualizar/:id", component: StudentView, meta: { auth: true, permission: { entity: "students", action: "read" } } },
+  { path: "/administrador/estudantes/editar/:id", component: StudentEdit, meta: { auth: true, permission: { entity: "students", action: "update" } } },
 
   // Cursos e Turmas
-  { path: "/administrador/cursos/listar", component: CourseList, meta: { auth: true } },
-  { path: "/administrador/turmas/listar", component: SchoolGroupsList, meta: { auth: true } },
+  { path: "/administrador/cursos/listar", component: CourseList, meta: { auth: true, permission: { entity: "courses", action: "read" } } },
+  { path: "/administrador/turmas/listar", component: SchoolGroupsList, meta: { auth: true, permission: { entity: "classes", action: "read" } } },
 
   // Ocorrências
-  { path: "/ocorrencias/ocorrencias/listar", component: IncidentsList, meta: { auth: true } },
-  { path: "/ocorrencias/ocorrencias/novo", component: IncidentsNew, meta: { auth: true } },
-  { path: "/ocorrencias/ocorrencias/visualizar/:id", component: IncidentsView, meta: { auth: true } },
-  { path: "/ocorrencias/ocorrencias/editar/:id", component: IncidentsEdit, meta: { auth: true } },
-  { path: "/ocorrencias/relatorio", component: IncidentsReport, meta: { auth: true } },
+  { path: "/ocorrencias/ocorrencias/listar", component: IncidentsList, meta: { auth: true, permission: { entity: "occurrences", action: "read" } } },
+  { path: "/ocorrencias/ocorrencias/novo", component: IncidentsNew, meta: { auth: true, permission: { entity: "occurrences", action: "create" } } },
+  { path: "/ocorrencias/ocorrencias/visualizar/:id", component: IncidentsView, meta: { auth: true, permission: { entity: "occurrences", action: "read" } } },
+  { path: "/ocorrencias/ocorrencias/editar/:id", component: IncidentsEdit, meta: { auth: true, permission: { entity: "occurrences", action: "update" } } },
+  { path: "/ocorrencias/relatorio", component: IncidentsReport, meta: { auth: true, permission: { entity: "occurrences", action: "read" } } },
 ]
 
 const router = createRouter({
@@ -55,15 +58,25 @@ const router = createRouter({
 })
 
 router.beforeEach(async (to, _from, next) => {
-  if (to.meta.auth) {
-    if (await isTokenValid()) {
-      next()
-    } else {
-      next({ path: "/" })
-    }
-  } else {
+  if (!to.meta.auth) {
     next()
+    return
   }
+
+  if (!(await isTokenValid())) {
+    next({ path: "/" })
+    return
+  }
+
+  await loadCurrentPermissions()
+
+  const permission = to.meta.permission
+  if (permission && !can(permission.entity, permission.action)) {
+    next({ path: "/sem-permissao" })
+    return
+  }
+
+  next()
 })
 
 export default router
