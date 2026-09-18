e5e615c9109b0cc46aae74be46ee9bc2a7596541
Luiz F. Picolo <luizpicolo@gmail.com>
2026-09-18 10:56:41 -0400

    test: ajustar cobertura de autenticação da API

--- backend/spec/requests/api/users_spec.rb
+++ backend/spec/requests/api/users_spec.rb
@@ -16,7 +16,6 @@ RSpec.describe 'Api::Users', type: :request do
   describe 'GET /api/users' do
     it 'lista usuários autenticados' do
       get '/api/users'
-
       expect(response).to have_http_status(:ok)
       body = JSON.parse(response.body)
       expect(body).to include('users', 'total')
@@ -27,7 +26,6 @@ RSpec.describe 'Api::Users', type: :request do
   describe 'GET /api/users/:id' do
     it 'retorna um usuário autenticado' do
       get "/api/users/#{user.id}"
-
       expect(response).to have_http_status(:ok)
       expect(JSON.parse(response.body).dig('user', 'id')).to eq(user.id)
     end
@@ -36,14 +34,12 @@ RSpec.describe 'Api::Users', type: :request do
   describe 'PUT /api/users/:id' do
     it 'atualiza os dados do usuário sem exigir nova senha' do
       put "/api/users/#{user.id}", params: { user: { name: 'Nome Atualizado', email: user.email } }
-
       expect(response).to have_http_status(:ok)
       expect(user.reload.name).to eq('Nome Atualizado')
     end
 
     it 'retorna erros de validação' do
       put "/api/users/#{user.id}", params: { user: { email: '' } }
-
       expect(response).to have_http_status(:unprocessable_entity)
       expect(JSON.parse(response.body)).to include('errors')
     end
@@ -51,16 +47,12 @@ RSpec.describe 'Api::Users', type: :request do
 
   describe 'DELETE /api/users/:id' do
     it 'remove outro usuário' do
-      expect do
-        delete "/api/users/#{user.id}"
-      end.to change(User, :count).by(-1)
-
+      expect { delete "/api/users/#{user.id}" }.to change(User, :count).by(-1)
       expect(response).to have_http_status(:no_content)
     end
 
     it 'impede que o usuário autenticado seja excluído' do
       delete "/api/users/#{admin.id}"
-
       expect(response).to have_http_status(:unprocessable_entity)
       expect(JSON.parse(response.body)['error']).to eq('Não é possível excluir o próprio usuário')
     end
