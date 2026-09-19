038b6d8f09bcf3a8501dcd67cd0da89d972d9332
Luiz Picolo <luizpicolo@gmail.com>
2026-09-19 14:29:05 -0400

    Adiciona versão thumb para redimensionamento de imagens

--- backend/app/uploaders/user_uploader.rb
+++ backend/app/uploaders/user_uploader.rb
@@ -13,6 +13,10 @@ class UserUploader < CarrierWave::Uploader::Base
     '/default.jpg'
   end
 
+  version :thumb do
+    process resize_to_fit: [200, 200]
+  end
+
   def extension_allowlist
     %w[jpg jpeg gif png]
   end
