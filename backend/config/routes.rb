1d6ea8f7133c2767b236f199f7803bdad54b4f98
Luiz F. Picolo <luizpicolo@gmail.com>
2026-09-18 22:39:41 -0400

    feat: expose occurrence report endpoint

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -26,6 +26,7 @@ Rails.application.routes.draw do
     resources :incidents, only: [:index, :show, :create, :update, :destroy] do
       collection { get :options }
     end
+    post 'report_incidents', to: 'report_incidents#create'
   end
 
   devise_for :user, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', registration: 'register', sign_up: 'signup' }, controllers: { sessions: 'sessions' }
