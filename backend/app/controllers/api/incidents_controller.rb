4a0f29011e364963d51e0f7cc66ff985602a085b
Luiz F. Picolo <luizpicolo@gmail.com>
2026-09-20 18:39:44 -0400

    Aplicar permissão de sanção também na API

--- backend/app/controllers/api/incidents_controller.rb
+++ backend/app/controllers/api/incidents_controller.rb
@@ -17,7 +17,7 @@ class Api::IncidentsController < ApplicationController
 
   def options
     authorize! :create, Incident
-    render json: { assistants: User.where(set_polo).order(:name).as_json(only: %i[id name email]), sectors: Sector.where(set_polo).order(:name).as_json(only: %i[id name email]), type_incidents: Incident::TypeIncident.order(:name).as_json(only: %i[id name]), student_duties: Incident::StudentDuty.where(status: true).order(:id).as_json(only: %i[id item]), prohibition_and_responsibilities: Incident::ProhibitionAndResponsibility.where(status: true).order(:id).as_json(only: %i[id item]), sanctions: Incident.sanctions.keys.map { |key| { value: key, label: I18n.t("enums.incident.sanction.#{key}", default: key.humanize) } } }
+    render json: { assistants: User.where(set_polo).order(:name).as_json(only: %i[id name email]), sectors: Sector.where(set_polo).order(:name).as_json(only: %i[id name email]), type_incidents: Incident::TypeIncident.order(:name).as_json(only: %i[id name]), student_duties: Incident::StudentDuty.where(status: true).order(:id).as_json(only: %i[id item]), prohibition_and_responsibilities: Incident::ProhibitionAndResponsibility.where(status: true).order(:id).as_json(only: %i[id item]), sanctions: can?(:sanction, Incident) ? Incident.sanctions.keys.map { |key| { value: key, label: I18n.t("enums.incident.sanction.#{key}", default: key.humanize) } } : [] }
   end
 
   def show
@@ -92,7 +92,10 @@ class Api::IncidentsController < ApplicationController
   end
 
   def incident_params
-    params.require(:incident).permit(:type_incident_id, :student_id, :date_incident, :sector_id, :assistant_id, :time_incident, :institution, :description, :soluction, :is_resolved, :visibility, :type_student, :sanction, student_ids: [], prohibition_and_responsibility_ids: [], student_duty_ids: [])
+    permitted = params.require(:incident).permit(:type_incident_id, :student_id, :date_incident, :sector_id, :assistant_id, :time_incident, :institution, :description, :soluction, :is_resolved, :visibility, :type_student, :sanction, student_ids: [], prohibition_and_responsibility_ids: [], student_duty_ids: [])
+    return permitted if can?(:sanction, Incident)
+
+    permitted.except(:soluction, :is_resolved, :sanction, :student_duty_ids, :prohibition_and_responsibility_ids)
   end
 
   def incident_json(incident)
