485dfcf0be1ad633d35f215b53940828ab37ba4c
Luiz F. Picolo <luizpicolo@gmail.com>
2026-09-20 17:59:27 -0400

    Add incident_student_duties to ActiveAdmin

---
+++ backend/app/admin/incident_student_duties.rb
@@ -0,0 +1,25 @@
+ActiveAdmin.register Incident::StudentDuty, as: 'Atividade do Aluno' do
+  menu label: 'Atividades do aluno'
+
+  permit_params :item, :status
+
+  index do
+    selectable_column
+    id_column
+    column :item
+    column :status
+    column :created_at
+    actions
+  end
+
+  filter :item
+  filter :status
+
+  form do |f|
+    f.inputs 'Atividade do aluno' do
+      f.input :item
+      f.input :status
+    end
+    f.actions
+  end
+end
