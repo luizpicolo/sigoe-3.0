397efda90ad61e0552ad010e76e99999654197e7
Luiz F. Picolo <luizpicolo@gmail.com>
2026-09-20 17:59:25 -0400

    Add incident_type_incidents to ActiveAdmin

---
+++ backend/app/admin/incident_type_incidents.rb
@@ -0,0 +1,25 @@
+ActiveAdmin.register Incident::TypeIncident, as: 'Tipo de Ocorrência' do
+  menu label: 'Tipos de ocorrência'
+
+  permit_params :name, :old_id
+
+  index do
+    selectable_column
+    id_column
+    column :name
+    column :old_id
+    column :created_at
+    actions
+  end
+
+  filter :name
+  filter :old_id
+
+  form do |f|
+    f.inputs 'Tipo de ocorrência' do
+      f.input :name
+      f.input :old_id
+    end
+    f.actions
+  end
+end
