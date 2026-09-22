384775eca57172da6432b9f7daaea756a72da459
Luiz F. Picolo <luizpicolo@gmail.com>
2026-09-22 13:05:02 -0400

    test: repair component smoke tests

--- sigoe-ui/tests/components/all-components.spec.js
+++ sigoe-ui/tests/components/all-components.spec.js
@@ -1,9 +1,31 @@
 import { describe, expect, it } from 'vitest'
+import { shallowMount } from '@vue/test-utils'
 
 const componentModules = import.meta.glob('../../src/components/**/*.vue', { eager: true })
 
-describe('Vue components', () => {
-  it.each(Object.entries(componentModules))('discovers %s', ([, module]) => {
-    expect(module).toBeDefined()
+const globalOptions = {
+  stubs: {
+    RouterLink: true,
+    RouterView: true,
+    FontAwesomeIcon: true,
+  },
+}
+
+describe('Componentes Vue', () => {
+  it.each(Object.entries(componentModules))('exporta o componente %s', ([, module]) => {
+    expect(module?.default).toBeDefined()
+  })
+
+  it.each(Object.entries(componentModules))('monta o componente %s', ([path, module]) => {
+    const component = module?.default
+    expect(component).toBeDefined()
+
+    const wrapper = shallowMount(component, {
+      global: globalOptions,
+      props: {},
+    })
+
+    expect(wrapper.exists()).toBe(true)
+    wrapper.unmount()
   })
 })
