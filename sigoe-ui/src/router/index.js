068107651235144544693039e88a4ec68574e1be
Luiz F. Picolo <luizpicolo@gmail.com>
2026-09-25 11:20:24 -0400

    feat: add mobile student and user routes

--- sigoe-ui/src/router/index.js
+++ sigoe-ui/src/router/index.js
@@ -10,6 +10,13 @@ import MobileCourseList from "@/views/mobile/courses/Index.vue"
 import MobileCourseForm from "@/views/mobile/courses/Form.vue"
 import MobileSchoolGroupList from "@/views/mobile/school-groups/Index.vue"
 import MobileSchoolGroupForm from "@/views/mobile/school-groups/Form.vue"
+import MobileStudentList from "@/views/mobile/students/Index.vue"
+import MobileStudentForm from "@/views/mobile/students/Form.vue"
+import MobileStudentShow from "@/views/mobile/students/Show.vue"
+import MobileUserList from "@/views/mobile/users/Index.vue"
+import MobileUserForm from "@/views/mobile/users/Form.vue"
+import MobileUserShow from "@/views/mobile/users/Show.vue"
+import MobileUserPermissions from "@/views/mobile/users/Permissions.vue"
 const routes = [
 {path:"/mobile/dashboard",component:MobileDashboard,meta:{auth:true,mobile:true,mobileTitle:"Dashboard"}},
 {path:"/mobile/incidents",component:MobileIncidentList,meta:{auth:true,mobile:true,mobileTitle:"Ocorrências",permission:{entity:"occurrences",action:"read"}}},
@@ -22,6 +29,15 @@ const routes = [
 {path:"/mobile/school-groups",component:MobileSchoolGroupList,meta:{auth:true,mobile:true,mobileTitle:"Turmas",permission:{entity:"classes",action:"read"}}},
 {path:"/mobile/school-groups/new",component:MobileSchoolGroupForm,meta:{auth:true,mobile:true,mobileTitle:"Nova turma",permission:{entity:"classes",action:"create"}}},
 {path:"/mobile/school-groups/:id/edit",component:MobileSchoolGroupForm,meta:{auth:true,mobile:true,mobileTitle:"Editar turma",permission:{entity:"classes",action:"update"}}},
+{path:"/mobile/students",component:MobileStudentList,meta:{auth:true,mobile:true,mobileTitle:"Estudantes",permission:{entity:"students",action:"read"}}},
+{path:"/mobile/students/new",component:MobileStudentForm,meta:{auth:true,mobile:true,mobileTitle:"Novo estudante",permission:{entity:"students",action:"create"}}},
+{path:"/mobile/students/:id",component:MobileStudentShow,meta:{auth:true,mobile:true,mobileTitle:"Detalhes do estudante",permission:{entity:"students",action:"read"}}},
+{path:"/mobile/students/:id/edit",component:MobileStudentForm,meta:{auth:true,mobile:true,mobileTitle:"Editar estudante",permission:{entity:"students",action:"update"}}},
+{path:"/mobile/users",component:MobileUserList,meta:{auth:true,mobile:true,mobileTitle:"Usuários",permission:{entity:"users",action:"read"}}},
+{path:"/mobile/users/new",component:MobileUserForm,meta:{auth:true,mobile:true,mobileTitle:"Novo usuário",permission:{entity:"users",action:"create"}}},
+{path:"/mobile/users/:id",component:MobileUserShow,meta:{auth:true,mobile:true,mobileTitle:"Detalhes do usuário",permission:{entity:"users",action:"read"}}},
+{path:"/mobile/users/:id/edit",component:MobileUserForm,meta:{auth:true,mobile:true,mobileTitle:"Editar usuário",permission:{entity:"users",action:"update"}}},
+{path:"/mobile/users/:id/permissions",component:MobileUserPermissions,meta:{auth:true,mobile:true,mobileTitle:"Permissões",permission:{entity:"users",action:"update",adminOnly:true}}},
 {path:"/",component:Login,meta:{auth:false}},{path:"/home",component:Home,meta:{auth:true}},{path:"/sem-permissao",component:Forbidden,meta:{auth:true}},
 {path:"/administrador/usuarios/listar",component:UserList,meta:{auth:true,permission:{entity:"users",action:"read"}}},{path:"/administrador/usuarios/visualizar/:id",component:UserView,meta:{auth:true,permission:{entity:"users",action:"read"}}},{path:"/administrador/usuarios/novo",component:UserForm,meta:{auth:true,permission:{entity:"users",action:"create"}}},{path:"/administrador/usuarios/editar/:id",component:UserForm,meta:{auth:true,permission:{entity:"users",action:"update"}}},{path:"/administrador/usuarios/visualizar/:id/permissoes",component:UserPermissions,meta:{auth:true,permission:{entity:"users",action:"update",adminOnly:true}}},{path:"/administrador/usuarios/trocar-senha",component:ChangePassword,meta:{auth:true}},
 {path:"/administrador/estudantes/listar",component:StudentList,meta:{auth:true,permission:{entity:"students",action:"read"}}},{path:"/administrador/estudantes/novo",component:StudentForm,meta:{auth:true,permission:{entity:"students",action:"create"}}},{path:"/administrador/estudantes/visualizar/:id",component:StudentView,meta:{auth:true,permission:{entity:"students",action:"read"}}},{path:"/administrador/estudantes/editar/:id",component:StudentForm,meta:{auth:true,permission:{entity:"students",action:"update"}}},
