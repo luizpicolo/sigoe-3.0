e1f169fb931ebca4db2bfdd42a47b8fb7f67970e
Luiz F. Picolo <luizpicolo@gmail.com>
2026-09-25 22:49:29 -0400

    Remove global API authentication callback

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -2,13 +2,12 @@
 
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
-  before_action :authenticate_user!
 
   rescue_from CanCan::AccessDenied do |_exception|
     redirect_back(fallback_location: root_path)
   end
 
   def authenticate_admin!
-    redirect_to new_user_session_path unless current_user.super_admin?
+    redirect_to new_user_session_path unless current_user&.super_admin?
   end
 end
