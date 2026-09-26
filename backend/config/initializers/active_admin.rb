47356dce9893c9dd26929139c465614736bc02bf
Luiz F. Picolo <luizpicolo@gmail.com>
2026-09-25 23:03:21 -0400

    fix: use User as ActiveAdmin current user

--- backend/config/initializers/active_admin.rb
+++ backend/config/initializers/active_admin.rb
@@ -72,6 +72,7 @@ ActiveAdmin.setup do |config|
   # This setting changes the method which Active Admin calls
   # within the application controller.
   config.authentication_method = :authenticate_admin!
+  config.current_user_method = :current_user
 
   # == User Authorization
   #
